19e78acd37680c18ab5798b415b813749a0afd56
Prasenjit Datta <prasenjit.datta1@gds.ey.com>
2026-02-23 16:17:27 +0530

    added unit test in pysdk/test.py

--- pysdk/test.py
+++ pysdk/test.py
@@ -1,8 +1,15 @@
+import sys
 from aimd import AIMD
 
 req = AIMD(3000, 8080)
-uid, token, exp = req.register_agent()
+if len(sys.argv) > 1:
+    assert req.port == 8080, "Port did not update correctly!"
+    assert req.exp_time == 3000, "Expiry time did not update correctly!"
+    print("Assertions passed ✔")
 
-print("UID:", uid)
-print("Token:", token)
-print("Expires:", exp)
+else:
+    uid, token, exp = req.register_agent()
+
+    print("UID:", uid)
+    print("Token:", token)
+    print("Expires:", exp)
